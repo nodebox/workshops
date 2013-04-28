72dc1c69930a9ae2b36395f70dab7a0160d57ac4
Frederik De Bleser <frederik@burocrazy.com>
2013-04-28 09:18:08 -0400

    Add hacky way of creating new posts.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from blog import views
 
 urlpatterns = patterns('',
     url(r'^$', views.redirect_to_latest, name='redirect_to_latest'),
+    url(r'^posts/$', views.post_list_by_user, name='post_list_by_user'),
+    url(r'^posts/new/$', views.post_create, name='post_create'),
     url(r'^(?P<blog_slug>[\w-]+)/$', views.post_list, name='post_list'),
     url(r'^(?P<blog_slug>[\w-]+)/about/$', views.blog_about, name='blog_about'),
     url(r'^(?P<blog_slug>[\w-]+)/(?P<username>[\w-]+)/$', views.post_list_by_author, name='post_list_by_author'),
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.template import RequestContext
+from django.utils.text import slugify
 
 from blog.models import Blog, Post
 
@@ -36,3 +39,39 @@ def post_detail(request, blog_slug, username, post_slug):
     blog = get_object_or_404(Blog, slug=blog_slug)
     post = get_object_or_404(Post, blog__slug=blog_slug, user__username=username, slug=post_slug)
     return render_to_response('blog/post_detail.html', {'blog': blog, 'post': post})
+
+
+@login_required
+def post_list_by_user(request):
+    posts = request.user.post_set.all()
+    return render_to_response('blog/post_list_by_user.html',
+                              {'user': request.user, 'posts': posts})
+
+
+class PostForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    body = forms.CharField(widget=forms.Textarea)
+
+
+@login_required
+def post_create(request):
+    blog = Blog.objects.get(slug='2013-montreal')
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = Post(blog=blog,
+                        user=request.user,
+                        title=form.cleaned_data['title'],
+                        slug=slugify(form.cleaned_data['title']),
+                        body=form.cleaned_data['body'])
+            post.save()
+            return redirect(post)
+    else:
+        form = PostForm()
+
+    return render_to_response('blog/post_create.html',
+                              {'user': request.user, 'blog': blog, 'form': form}, context_instance=RequestContext(request))
+
+
+
+
